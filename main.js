992ed21975ea0936578b849054ad7323a998787a
Kayla <kayla.d.trinh@gmail.com>
2018-12-03 00:59:14 -0500

    Fix smooth scroll

---
+++ main.js
@@ -0,0 +1,36 @@
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  });
+});
+
+var acc = document.getElementsByClassName("job");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+/*$(".job").each(function() {
+
+  $(this.click(function() {
+
+    $(this).toggleClass("active");
+
+    $(this).next().slideToggle(200);
+
+  });
+});
+*/
